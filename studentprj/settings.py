c3aa8d7c6d96a8c7fa0e003c8d6bb34526571d19
gulshan25 <g.r.tanny@gmail.com>
2021-11-24 17:14:19 +0600

    send mail

--- studentprj/settings.py
+++ studentprj/settings.py
@@ -138,5 +138,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'pythonbitm0105@daffodil.ac'
+EMAIL_HOST_USER = 'bitmbatch@gmail.com'
 EMAIL_HOST_PASSWORD = '01746511536'
